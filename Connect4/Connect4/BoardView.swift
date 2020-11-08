b9c8ce3998a1c4c968e2f8135b085f3405bce7ab
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-08 09:12:44 -0500

    made board background blue

--- Connect4/Connect4/BoardView.swift
+++ Connect4/Connect4/BoardView.swift
@@ -16,8 +16,12 @@ class BoardView: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
-        NSColor.white.setFill()
-        bounds.fill()
+        drawBoard()
+    }
+    
+    func drawBoard() {
+        #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).setFill()
+        NSBezierPath(roundedRect: bounds, xRadius: 20, yRadius: 20).fill()
         
         for row in 0..<6 {
             for col in 0..<7 {
@@ -32,12 +36,15 @@ class BoardView: NSView {
     
     func drawCircle(x: CGFloat, y: CGFloat, radius: CGFloat) {
         let circleRect = NSRect(x: x, y: y, width: 2 * radius, height: 2 * radius)
-//        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setStroke()
+//        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).setStroke()
 //        NSBezierPath(rect: circleRect).stroke()
         
         let path = NSBezierPath(ovalIn: circleRect)
+        #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setFill()
+        path.fill()
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         path.stroke()
+
     }
     
 }

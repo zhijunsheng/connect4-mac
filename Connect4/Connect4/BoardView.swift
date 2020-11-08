83aea06b6ff813652fcadad144fc4a06369c30e7
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-08 00:13:16 -0500

    drew 6x7 game board of connect4

--- Connect4/Connect4/BoardView.swift
+++ Connect4/Connect4/BoardView.swift
@@ -8,6 +8,10 @@
 import Cocoa
 
 class BoardView: NSView {
+    let boardX: CGFloat = 30
+    let boardY: CGFloat = 20
+    let cellSide: CGFloat = 70
+    let radius: CGFloat = 25
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
@@ -15,9 +19,21 @@ class BoardView: NSView {
         NSColor.white.setFill()
         bounds.fill()
         
-        let circleRect = NSRect(x: 30, y: 20, width: 50, height: 50)
-        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setStroke()
-        NSBezierPath(rect: circleRect).stroke()
+        for row in 0..<6 {
+            for col in 0..<7 {
+                drawCircleAt(col: col, row: row)
+            }
+        }
+    }
+    
+    func drawCircleAt(col: Int, row: Int) {
+        drawCircle(x: boardX + CGFloat(col) * cellSide, y: boardY + CGFloat(row) * cellSide, radius: radius)
+    }
+    
+    func drawCircle(x: CGFloat, y: CGFloat, radius: CGFloat) {
+        let circleRect = NSRect(x: x, y: y, width: 2 * radius, height: 2 * radius)
+//        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setStroke()
+//        NSBezierPath(rect: circleRect).stroke()
         
         let path = NSBezierPath(ovalIn: circleRect)
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()

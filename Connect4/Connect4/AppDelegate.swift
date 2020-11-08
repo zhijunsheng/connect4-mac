2af77ab5af1a6c5242feda7165bb681a437fb23c
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-07 23:14:14 -0500

    created macOS xcode proj

---
+++ Connect4/Connect4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-07.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Connect4/Connect4/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

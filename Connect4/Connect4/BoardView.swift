af536c69811985bb2a86c635395afea0d3e71944
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-07 23:50:33 -0500

    drew our first circle on board

--- Connect4/Connect4/BoardView.swift
+++ Connect4/Connect4/BoardView.swift
@@ -14,6 +14,14 @@ class BoardView: NSView {
 
         NSColor.white.setFill()
         bounds.fill()
+        
+        let circleRect = NSRect(x: 30, y: 20, width: 50, height: 50)
+        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setStroke()
+        NSBezierPath(rect: circleRect).stroke()
+        
+        let path = NSBezierPath(ovalIn: circleRect)
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        path.stroke()
     }
     
 }

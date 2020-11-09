afbcdcff8ebcb3d27adfa459ecd819c34f00e50d
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-09 17:27:08 -0500

    tried 3 programming concepts in Xcode PlayGround named SwiftDay01Playground

---
+++ SwiftDay01Playground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+var str = "Hello, playground"
+str = "Hello, friends"
+
+// concept 1
+print("Hello, 1")
+print("Hello, 2")
+print("Hello, 3")
+
+// concept 2
+let x = 50
+
+if x % 2 == 0 {
+    print("x is even")
+} else {
+    print("x is odd")
+}
+
+// concept 3
+
+for i in 0...30 {
+    print(i)
+}
+

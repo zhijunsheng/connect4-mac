3605b131b7781e26c610842a56c91bc93b50db92
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-09 20:52:49 -0500

    played with for loops on an int array

--- Connect4/Connect4Tests/ArrayTests.swift
+++ Connect4/Connect4Tests/ArrayTests.swift
@@ -10,7 +10,24 @@ import XCTest
 class ArrayTests: XCTestCase {
 
     func testIntArray() {
-        let intArr: [Int] = [2, 3, 5, 7, 11]
+        var intArr: [Int] = [2, 3, 5, 7, 11]
         XCTAssertEqual(11, intArr[intArr.count - 1])
+        
+        intArr.append(13) // [2, 3, 5, 7, 11, 13]
+        XCTAssertEqual(13, intArr[intArr.count - 1])
+        
+//        for _ in 0..<intArr.count {
+//            intArr.remove(at: 0)
+//        }
+//        intArr.removeAll()
+        intArr = [1, 2, 3, 4, 5]
+        
+        for i in 0..<intArr.count {
+            print("i = \(i), intArr[i] = \(intArr[i])")
+        }
+        
+        for intNum in intArr {
+            print(intNum)
+        }
     }
 }

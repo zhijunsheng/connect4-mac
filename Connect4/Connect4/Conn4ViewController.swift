8b125860d697187edca5d626426439be93ad7251
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-12 09:16:42 -0500

    let Conn4ViewController adopt 3 MultipeerConnectivity related protocols

--- Connect4/Connect4/Conn4ViewController.swift
+++ Connect4/Connect4/Conn4ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import MultipeerConnectivity
 
 class Conn4ViewController: NSViewController {
 
@@ -26,7 +27,42 @@ class Conn4ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension Conn4ViewController: MCBrowserViewControllerDelegate {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        
+    }
+}
+
+extension Conn4ViewController: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        
+    }
+}
+
+extension Conn4ViewController: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+}

290ad585ac9ce4b9a8bbf1974f58e5ba6211e562
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-15 18:51:10 -0500

    printed out an empty conn4 board using in a test case

--- Connect4/Connect4/Conn4Board.swift
+++ Connect4/Connect4/Conn4Board.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
-struct Conn4Board {
+struct Conn4Board: CustomStringConvertible {
     var piecesBox: [Conn4Piece] = []
     
     mutating func dropPieceAt(col: Int) {
         piecesBox.append(Conn4Piece(col: col, row: 0, player: Conn4Player.red))
+    }
+    
+    var description: String {
+        var desc: String = ""
+        
+        for _ in 0..<6 {
+            for _ in 0..<7 {
+                desc += " ."
+            }
+            desc += "\n"
+        }        
+        
+        return desc
     }
 }
---
+++ Connect4/Connect4Tests/Conn4BoardTests.swift
@@ -0,0 +1,19 @@
+//
+//  Conn4BoardTests.swift
+//  Connect4Tests
+//
+//  Created by Golden Thumb on 2020-11-15.
+//
+
+import XCTest
+@testable import Connect4
+
+class Conn4BoardTests: XCTestCase {
+
+    func testDescription() {
+        let conn4Board = Conn4Board()
+        
+        print(conn4Board)
+    }
+
+}
